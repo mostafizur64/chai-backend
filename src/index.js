a1d32338e37eea02019e199ef4c619a622dd02a2
tarafdar16 <imrantarafdar16@gmail.com>
2024-03-28 00:13:37 +0600

    api response and error handling

--- src/index.js
+++ src/index.js
@@ -6,29 +6,21 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    
+    app.on("error", () => {
+      console.log("ERROR :", error);
+      throw error;
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is Running port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
 
 // import express from "express";
 // const app = express();
